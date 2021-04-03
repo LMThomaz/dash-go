b7866eba01eb9af661c667ed2a271ed690cfbea6
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-04-03 12:54:41 -0300

    Finalizando Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -43,6 +47,34 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize={20} />
+      </Flex>
+
+      <Flex align='center' ml='auto'>
+        <HStack
+          spacing='8'
+          mx='8'
+          pr='8'
+          py='1'
+          color='gray.300'
+          borderRightWidth={1}
+          borderColor='gray.700'
+        >
+          <Icon as={RiNotificationLine} fontSize={20} />
+          <Icon as={RiUserAddLine} fontSize={20} />
+        </HStack>
+        <Flex align='center'>
+          <Box mr='4' textAlign='right'>
+            <Text>Leonardo Thomaz</Text>
+            <Text color='gray.300' fontSize='small'>
+              leonardo.thomaz@hotmail.com
+            </Text>
+          </Box>
+          <Avatar
+            size='md'
+            name='Leonardo Thomaz'
+            src='https://github.com/LMThomaz.png'
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
