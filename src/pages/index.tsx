dd069dd8e93de90912c458486676fbae2b53bc78
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-04-03 12:27:32 -0300

    Página: Sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,65 @@
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from '@chakra-ui/react';
+
 export default function Home() {
-  return <h1>Dash Go</h1>;
+  return (
+    <Flex
+      width='100vw'
+      height='100vh'
+      alignItems='center'
+      justifyContent='center'
+    >
+      <Flex
+        as='form'
+        width='100%'
+        maxWidth={360}
+        bg='gray.800'
+        p='8'
+        borderRadius={8}
+        flexDir='column'
+      >
+        <Stack spacing='4'>
+          <FormControl>
+            <FormLabel htmlFor='email'>E-mail</FormLabel>
+            <Input
+              id='email'
+              name='email'
+              type='email'
+              focusBorderColor='pink.500'
+              bgColor='gray.900'
+              variant='filled'
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+              size='lg'
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor='password'>Senha</FormLabel>
+            <Input
+              id='password'
+              name='password'
+              type='password'
+              focusBorderColor='pink.500'
+              bgColor='gray.900'
+              variant='filled'
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+              size='lg'
+            />
+          </FormControl>
+        </Stack>
+        <Button type='submit' mt='6' colorScheme='pink' size='lg'>
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
