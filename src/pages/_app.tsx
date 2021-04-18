3b5e94410666b0b38e87a1c19684ff1b1a69af90
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-04-18 15:14:36 -0300

    Configurando MirageJS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { makeServer } from '../services/mirage';
 import { theme } from '../styles/theme';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
